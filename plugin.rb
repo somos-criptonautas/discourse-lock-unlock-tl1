d9f6217d3bc07f8a8a782896a3a8ffb03750d70f
satoshinotdead <dev@criptonautas.co>
2025-07-07 23:29:04 -0500

    update

--- plugin.rb
+++ plugin.rb
@@ -1,44 +1,44 @@
 # plugin.rb
-# name: discourse-lock-tl1
-# about: Locks new group members to Trust Level 1 via Automation, with error logging
-# version: 0.2
+# name: discourse-lock-unlock-tl
+# about: Locks new group members to a selectable Trust Level via Automation, with error logging
+# version: 0.3
 # authors: satonotdead
 
 after_initialize do
   # Proceed only if the Automation plugin is loaded
   if defined?(DiscourseAutomation)
 
-    DiscourseAutomation::Scriptable.add("lock_group_tl1") do
+    DiscourseAutomation::Scriptable.add("lock_group_trust_level") do
       version 1
 
-      # Admin field to select the target group
+      # Admin fields
       field :target_group, component: :group, required: true
+      field :trust_level, component: :trust_level, required: true
 
       # Run this script when a user is added to any group
       triggerables %i[user_added_to_group]
 
       script do |context, fields, automation|
-        user     = context["user"]
+        user = context["user"]
         group_id = fields.dig("target_group", "value").to_i
+        trust_level = fields.dig("trust_level", "value").to_i
 
         # Only proceed if the user is in the chosen group
         if user.groups.pluck(:id).include?(group_id)
           begin
-            # Attempt to lock trust level at 1
-            user.update!(manual_locked_trust_level: 1)
+            # Attempt to lock trust level
+            user.update!(manual_locked_trust_level: trust_level)
             Rails.logger.info(
-              "DiscourseLockTL1: Successfully locked TL1 for user ##{user.id} "\
+              "DiscourseLockTL: Successfully locked TL#{trust_level} for user ##{user.id} " \
               "(#{user.username}) in group ##{group_id}"
             )
           rescue StandardError => e
             # Log error details without aborting the automation
             Rails.logger.error(
-              "DiscourseLockTL1: Error locking TL1 for user ##{user.id} "\
+              "DiscourseLockTL: Error locking TL#{trust_level} for user ##{user.id} " \
               "(#{user.username}) in group ##{group_id}: #{e.class} – #{e.message}"
             )
-            Rails.logger.error(
-              e.backtrace.take(10).join("\n")
-            )
+            Rails.logger.error(e.backtrace.take(10).join("\n"))
           end
         end
       end
